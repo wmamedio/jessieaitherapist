bf83ff00e1d209aac25350b131a77256023b232d
Weverson Mamedio <weverson@wmamedio.com.br>
2024-05-24 16:28:28 +0200

    text changes

--- my-svelte-app/src/routes/+page.svelte
+++ my-svelte-app/src/routes/+page.svelte
@@ -1,4 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<h2>Just testing 2.</h2>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
+<h2>Hi there!</h2>
+<h1>I'm Jessie! Your AI Therapist online.</h1>
+<p>I'm here to help you set meaningful goals and overcome your fears. Together, we'll define your dreams, set clear objectives, and build productive habits. Start your journey now by creating your account. Let's achieve your dreams together!</p>
