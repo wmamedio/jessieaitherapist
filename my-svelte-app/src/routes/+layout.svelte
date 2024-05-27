bb65b460ccd5e9c439da6339e8c7ce933c618cb6
Weverson Mamedio <weverson@wmamedio.com.br>
2024-05-27 17:30:15 +0200

    google analytics cod

--- my-svelte-app/src/routes/+layout.svelte
+++ my-svelte-app/src/routes/+layout.svelte
@@ -15,8 +15,16 @@
 <!-- Favicon -->
 <link rel="icon" href="/favicon.ico">
 
-<!-- Tailwind CSS -->
-<link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-1N106JM0M8"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-1N106JM0M8');
+</script>
+
 </svelte:head>
 
 <slot />
