ba728d5adbfa76170317cf7d106e41977fcdb287
Weverson Mamedio <weverson@wmamedio.com.br>
2024-06-01 12:17:17 +0200

    html content from bubble + images

--- my-svelte-app/src/routes/blog/+page.svelte
+++ my-svelte-app/src/routes/blog/+page.svelte
@@ -13,13 +13,13 @@
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
         {#each data.posts as post}
         <div class="bg-white rounded-lg shadow-lg overflow-hidden post">
-            <!-- <img class="w-full h-48 object-cover" src={"https:" + post.image_image} alt={post.title_text} /> -->
+            {#if post.image_image}
+                <img class="w-full h-48 object-cover" src={"https:" + post.image_image} alt={post.title_text} />
+            {/if}
             <div class="p-6">
-                <h2 class="text-xl font-semibold mb-2">{post.title_text}</h2>
-                <p class="text-gray-700 mb-2"><strong>Author:</strong> {post.author_name_text}</p>
-                <p class="text-gray-500 mb-4"><strong>Bio:</strong> {post.author_bio_text}</p>
-                <p class="text-gray-700 mb-4">{post.content_text}</p>
-                <a href={`/blog/${post.title_text.toLowerCase().replace(/\s+/g, '-')}`}>Read More</a>
+                <a class="text-xl font-semibold mb-2" href={`/blog/${post.title_text.toLowerCase().replace(/\s+/g, '-')}`}>{post.title_text}</a>
+                <!-- <p class="text-gray-700 mb-2"><strong>Author:</strong> {post.author_name_text}</p>
+                <p class="text-gray-500 mb-4"><strong>Bio:</strong> {post.author_bio_text}</p> -->
             </div>
         </div>
         {/each}
--- my-svelte-app/src/routes/blog/[name]/+page.svelte
+++ my-svelte-app/src/routes/blog/[name]/+page.svelte
@@ -2,18 +2,21 @@
     export let data;
 </script>
 
-<div class="p-8">
-    <div class="mb-4">
-        <a href="/blog" class="text-sm font-semibold leading-6 text-gray-900">Go Back</a>
-    </div>
-    
-    <div class="bg-white rounded-lg shadow-lg overflow-hidden post">
-        <!-- <img class="w-full h-48 object-cover" src={"https:" + data.post.image_image} alt={data.post.title_text} /> -->
-        <div class="p-6">
-            <h2 class="text-xl font-semibold mb-2">{data.post.title_text}</h2>
-            <p class="text-gray-700 mb-2"><strong>Author:</strong> {data.post.author_name_text}</p>
-            <p class="text-gray-500 mb-4"><strong>Bio:</strong> {data.post.author_bio_text}</p>
-            <p class="text-gray-700">{data.post.content_text}</p>
+<div class="p-8 flex justify-center">
+    <div class="w-full max-w-2xl">
+        <div class="mb-4">
+            <a href="/blog" class="text-sm font-semibold leading-6 text-gray-900">Go Back</a>
+        </div>
+        
+        <div class="bg-white rounded-lg shadow-lg overflow-hidden post">
+            {#if data.post.image_image}
+                <img class="w-full h-48 object-cover" src={"https:" + data.post.image_image} alt={data.post.title_text} />
+            {/if}
+            <div class="p-6">
+                <div class="prose max-w-none mb-8">{@html data.post.content_text}</div>
+                <p class="text-gray-700 mb-2"><strong>Author:</strong> {data.post.author_name_text}</p>
+                <p class="text-gray-700"><strong>Bio:</strong> {data.post.author_bio_text}</p>
+            </div>
         </div>
     </div>
 </div>
