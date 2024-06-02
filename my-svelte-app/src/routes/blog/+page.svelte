551327fd58d6a03795a042213c1ba4f0844e4860
Weverson Mamedio <weverson@wmamedio.com.br>
2024-06-02 08:58:28 +0200

    lazy load style

--- my-svelte-app/src/routes/blog/+page.svelte
+++ my-svelte-app/src/routes/blog/+page.svelte
@@ -15,15 +15,26 @@
         {#each data.posts as post}
         <div class="bg-white rounded-lg shadow-lg overflow-hidden post">
             {#if post.image_image}
-                <img use:lazyLoad class="w-full h-48 object-cover" data-src={"https:" + post.image_image} alt={post.title_text} />
+            <img use:lazyLoad class="w-full h-48 object-cover lazy" data-src={"https:" + post.image_image} alt={post.title_text} />
             {/if}
             <div class="p-6">
                 <a class="text-xl font-semibold mb-2" href={`/blog/${post.title_text.toLowerCase().replace(/\s+/g, '-')}`}>{post.title_text}</a>
                 <!-- <p class="text-gray-700 mb-2"><strong>Author:</strong> {post.author_name_text}</p>
-                <p class="text-gray-500 mb-4"><strong>Bio:</strong> {post.author_bio_text}</p> -->
+                    <p class="text-gray-500 mb-4"><strong>Bio:</strong> {post.author_bio_text}</p> -->
             </div>
         </div>
         {/each}
     </div>
     {/if}
 </div>
+
+<style>
+    .lazy {
+        opacity: 0;
+        transition: opacity 0.5s;
+    }
+    
+    img:not(.lazy) {
+        opacity: 1;
+    }
+</style>
