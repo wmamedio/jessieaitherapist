d1050a0fe048e3be01119222b2ee51b9ffe2bd40
Weverson Mamedio <weverson@wmamedio.com.br>
2024-06-13 18:23:39 +0200

    update links to Stripe spanish account

--- my-svelte-app/src/routes/+page.svelte
+++ my-svelte-app/src/routes/+page.svelte
@@ -19,7 +19,7 @@
         if (urlParams.get('mode') === 'test') {
             const links = document.getElementsByClassName('stripe-link');
             for (let i = 0; i < links.length; i++) {
-                links[i].href = 'https://buy.stripe.com/test_14k3enekB3JLaZ25kk';
+                links[i].href = 'https://buy.stripe.com/test_9AQ7wg3XS2xG6Iw3ce';
             }
         }
     });
@@ -64,7 +64,7 @@
         {/each}
         </div> -->
         <div class="hidden lg:flex lg:flex-1 lg:justify-end">
-            <a href="https://buy.stripe.com/aEUbMB1vC5m73UA4gg" class="stripe-link text-sm font-semibold leading-6 text-gray-900">
+            <a href="https://buy.stripe.com/14keXA401fEwaGc5km" class="stripe-link text-sm font-semibold leading-6 text-gray-900">
                 Log in <span aria-hidden="true">&rarr;</span>
             </a>
         </div>
@@ -107,7 +107,7 @@
                 </div> -->
                 <div class="py-6">
                     <a
-                    href="https://buy.stripe.com/aEUbMB1vC5m73UA4gg"
+                    href="https://buy.stripe.com/14keXA401fEwaGc5km"
                     class="stripe-link -mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                     >
                     Log in
@@ -134,7 +134,7 @@
             <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">I'm Jessie!<br/>Your Online Therapist AI.</h1>
             <p class="mt-6 text-lg leading-8 text-gray-600">I'm here to help you set meaningful goals and overcome your fears. Together, we'll define your dreams, set clear objectives, and build productive habits. Start your journey now by creating your account. Let's achieve your dreams together!</p>
             <div class="mt-10 flex items-center justify-center gap-x-6">
-                <a href="https://buy.stripe.com/aEUbMB1vC5m73UA4gg" class="stripe-link rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get started</a>
+                <a href="https://buy.stripe.com/14keXA401fEwaGc5km" class="stripe-link rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get started</a>
                 <a href="/blog" class="text-sm font-semibold leading-6 text-gray-900">Learn more<span aria-hidden="true">→</span></a> 
             </div>
         </div>
