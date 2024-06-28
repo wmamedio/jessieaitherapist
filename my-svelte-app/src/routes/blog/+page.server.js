4c0e6bb2c14a6e91d3a8a3e5d62ccd40386abf87
Weverson Mamedio <weverson@wmamedio.com.br>
2024-06-28 20:59:12 +0200

    updated code to get >100 posts

--- my-svelte-app/src/routes/blog/+page.server.js
+++ my-svelte-app/src/routes/blog/+page.server.js
@@ -2,10 +2,28 @@ export const prerender = true;
 
 export async function load({ fetch }) {
   try {
-    const response = await fetch('https://simplifying.bubbleapps.io/version-test/api/1.1/obj/jtblogpost');
-    const data = await response.json();
-    if (data.response.results.length > 0) {
-      return { posts: data.response.results };
+    const apiUrl = 'https://simplifying.bubbleapps.io/version-test/api/1.1/obj/jtblogpost';
+
+    let allPosts = [];
+    let cursor = 0;
+    let hasMore = true;
+
+    while (hasMore) {
+      const response = await fetch(`${apiUrl}?limit=100&cursor=${cursor}`);
+      const data = await response.json();
+
+      if (data.response.results.length > 0) {
+        allPosts = allPosts.concat(data.response.results);
+        // Update cursor to the next page's start point
+        cursor = cursor + data.response.results.length;
+        hasMore = data.response.remaining > 0;
+      } else {
+        hasMore = false;
+      }
+    }
+
+    if (allPosts.length > 0) {
+      return { posts: allPosts };
     } else {
       throw new Error('Failed to fetch posts or no posts available');
     }
--- my-svelte-app/src/routes/blog/[name]/+page.server.js
+++ my-svelte-app/src/routes/blog/[name]/+page.server.js
@@ -2,11 +2,27 @@ export const prerender = true;
 
 export async function load({ params, fetch }) {
     const { name } = params;
-    const response = await fetch('https://simplifying.bubbleapps.io/version-test/api/1.1/obj/jtblogpost');
-    const data = await response.json();
-    
-    if (data.response.results.length > 0) {
-        const post = data.response.results.find(post => post.title_text.toLowerCase().replace(/:/g, '').replace(/\s+/g, '-') === name);
+    const apiUrl = 'https://simplifying.bubbleapps.io/version-test/api/1.1/obj/jtblogpost';
+
+    let allPosts = [];
+    let cursor = 0;
+    let hasMore = true;
+
+    while (hasMore) {
+        const response = await fetch(`${apiUrl}?limit=100&cursor=${cursor}`);
+        const data = await response.json();
+
+        if (data.response.results.length > 0) {
+            allPosts = allPosts.concat(data.response.results);
+            cursor = data.response.cursor + data.response.results.length;
+            hasMore = data.response.remaining > 0;
+        } else {
+            hasMore = false;
+        }
+    }
+
+    if (allPosts.length > 0) {
+        const post = allPosts.find(post => post.title_text.toLowerCase().replace(/:/g, '').replace(/\s+/g, '-') === name);
         if (post) {
             return { post };
         } else {
