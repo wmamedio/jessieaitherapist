7a07c3ed8b8dbcd945e18c34c566e6646e27a2f4
Weverson Mamedio <weverson@wmamedio.com.br>
2024-08-08 08:26:47 +0200

    Added backlinks to footer

--- my-svelte-app/src/routes/Footer.svelte
+++ my-svelte-app/src/routes/Footer.svelte
@@ -24,7 +24,10 @@
         <div class="mt-12 border-t border-gray-700 pt-8 text-center">
             <p class="text-sm text-gray-400">&copy; 2024 Jessie Therapist. All rights reserved.</p>
         </div>
+        <p class="text-center text-gray-600 text-xs mt-2">
+            <a href="https://www.wmamedio.com/" class="text-gray-600 text-xs hover:text-gray-600 mt-4" target="_blank" rel="noopener">Developed with Bubble.io & Svelte by Weverson Mamédio</a> 
+            @ 
+            <a href="https://uai.agency/" class="text-gray-600 text-xs hover:text-gray-600 mt-4" target="_blank" rel="noopener">UAI Agency</a>
+        </p>
     </div>
 </section>
-
-
