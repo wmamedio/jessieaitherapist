02f43c1f76b58ad83752f157ebbc8ec9a5e7e9a2
Weverson Mamédio <weverson@wmamedio.com.br>
2025-06-27 10:54:22 +0200

    removed stripe link from login

--- my-svelte-app/src/routes/Header.svelte
+++ my-svelte-app/src/routes/Header.svelte
@@ -39,7 +39,7 @@
             {/each}
         </div>
         <div class="hidden lg:flex lg:flex-1 lg:justify-end">
-            <a href="https://buy.stripe.com/fZecNOcbwfFLfD2fYY" class="stripe-link text-sm font-semibold leading-6 text-gray-900">Log in <span aria-hidden="true">&rarr;</span></a>
+            <a href="/" class="stripe-link text-sm font-semibold leading-6 text-gray-900">Log in <span aria-hidden="true">&rarr;</span></a>
         </div>
     </nav>
     {#if mobileMenuOpen}
@@ -67,7 +67,7 @@
                         {/each}
                     </div>
                     <div class="py-6">
-                        <a href="https://buy.stripe.com/fZecNOcbwfFLfD2fYY" class="stripe-link -mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                        <a href="/" class="stripe-link -mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
                             Log in
                         </a>
                     </div>
